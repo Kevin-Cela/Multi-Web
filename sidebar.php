d0b9baa6c7a5623e5763afa3543d1eeebe0409aa
lulryeheej <erdiscelahg@gmail.com>
2023-07-17 14:32:32 +0200

    Created dicka sample

--- sidebar.php
+++ sidebar.php
@@ -3,19 +3,29 @@
 .sidenav {
   height: 100%;
   width: 300px;
-  background-color: #111;
+  background-color: #BFAAAA;
   padding-top: 20px;
   float:right;
 }
 .border{
     color: beige;
     border: 1;
+    border-color: #463E3E;
+    border-style:solid;
+    width: 200px;
+    height: 200px;
+   text-align: center;
+   margin-left: 50px; 
+}
+.border2{
+    color: beige;
+    border: 1;
     border-color: azure;
     border-style:double;
+    width: 70%;
+    margin-left: 25px; 
+
 }
-
-
-
 </style>
 </head>
 <body>
@@ -23,8 +33,22 @@
 <div class="sidenav">
     <div class="border">
 <h3>News 1</h3>
-<div>
-    <p></p>
+<div class="border2">
+    <p>A multipurpose web app that will include small projects</p>
+</div>
+</div>
+<br>
+<div class="border">
+<h3>News 1</h3>
+<div class="border2">
+    <p>A multipurpose web app that will include small projects</p>
+</div>
+</div>
+<br>
+<div class="border">
+<h3>News 1</h3>
+<div class="border2">
+    <p>A multipurpose web app that will include small projects</p>
 </div>
 </div>
 </div>
