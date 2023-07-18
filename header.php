633dbac1c559afd282a1f5b0dfd20ca1e29dfd0f
lulryeheej <erdiscelahg@gmail.com>
2023-07-18 18:00:50 +0200

    updated files

--- header.php
+++ header.php
@@ -3,34 +3,56 @@
 <head>
     <style>
         .navbar {
-            display: flex;
-            height: 40px;
             background-color: #8E0F0F;
-            font-size: 2rem;
+            display: grid;
+            grid-template-rows: 90%;
+            column-gap: 5px;
+            justify-content: center;
+            padding: 10px 10px;
+            box-sizing: border-box;
+            grid-template-columns: 100px 100px 100px 100px 100px;
+            column-gap: 15px;
+
         }
 
-        .menu {
-            font-family: 'Franklin Gothic Medium',
-                'Arial Narrow',
-                Arial, sans-serif;
-        }
-
-
-        a {
+        .navbar a {
+            float: left;
+            color: #f2f2f2;
+            padding: 14px 16px;
             text-decoration: none;
-            color: #fff;
-            padding: 0 4rem;
+            font-size: 20px;
         }
+
+        .navbar a:hover {
+            background-color: #ddd;
+            color: #8E0F0F;
+        }
+
+        .navtext {
+            /* 
+        nuk di si ta ve tekstin ne mes po brenda nje cell te gridit,shif si ka 
+          ngel ajo fjala logo majtas edhe jo nmes.*/
+          text-align: kte timin;
+        }
+
     </style>
 </head>
 
 <body>
     <div class="navbar">
-        <div class="menu">
+        <div class="navtext">
             <a href="#">pipupup</a>
+        </div>
+        <div class="navtext">
             <a href="#">dicka tjt</a>
+        </div>
+        <div class="navtext">
             <a href="#">logo</a>
+        </div>
+        <div class="navtext">
             <a href="#">dicka tjt</a>
+        </div>
+        <div class="navtext">
             <a href="#">dicka tjt</a>
         </div>
     </div>
